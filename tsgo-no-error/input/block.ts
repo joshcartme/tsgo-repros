b8170ff896e6c5f5eab5a6203c3735c7cc42e75c
Josh C <764456+joshcartme@users.noreply.github.com>
2026-04-16 10:39:57 -0700

    Example of how to get tsgo to report this

--- tsgo-no-error/input/block.ts
+++ tsgo-no-error/input/block.ts
@@ -1,4 +1,11 @@
 import type { SectionApi } from "./section";
+/* if you uncomment this, and comment out the line above, tsgo reports the error */
+// import type { Elements } from "./elements";
+
+// export interface SectionApi {
+//   type: "section";
+//   elements: Elements;
+// }
 
 export interface BlockApi {
 	type: "rich_text";
